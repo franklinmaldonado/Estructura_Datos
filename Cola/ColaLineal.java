a91adfae412c49a4ae0a895784f736aca6fe93c4
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2021-07-20 10:45:43 -0500

    Colas

---
+++ Cola/ColaLineal.java
@@ -0,0 +1,48 @@
+package Cola;
+
+public class ColaLineal {
+    private static fin int MAXTAMQ = 39;
+    protected int frente;
+    protected int fin;
+    protected TipoDeDato [] listaCola;
+
+    public ColaLineal() {
+         frente = 0;
+         fin = -1;
+         listaCola = new TipoDeDato [MAXTAMQ];
+    }
+
+    public void insertar(TipoDeDato elemento) throws Exception {
+         if (!colaLlena()) {
+              listaCola[++fin] = elemento;
+         }else
+              throw new Exception("Overflow en la cola");
+    }
+
+    public TipoDeDato quitar() throws Exception {
+         if (!colaVacia()) {
+              return listaCola[frente++];
+         }else
+              throw new Exception("Cola vacia ");
+    }
+
+    public void borrarCola() {
+         frente = 0;
+         fin = -1;
+    }
+
+    public TipoDeDato frenteCola() throws Exception {
+         if (!colaVacia()) {
+              return listaCola[frente];
+         }else
+              throw new Exception("Cola vacia ");
+    }
+
+    public boolean colaVacia() {
+         return frente > fin;
+    }
+
+    public boolean colaLlena() {
+        return fin == MAXTAMQ - 1;
+    }
+}
