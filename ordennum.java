b93c2e7658d6fbc3183206e3a4c20a47e18872d6
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2021-07-26 11:48:01 -0500

    Prueba

--- ordennum.java
+++ ordennum.java
@@ -1,30 +1,75 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ordennum {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int op;
+        int a[][], fila, columna;
+        String continuar;
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Ingrese la dimension del arreglo: ");
-        int tam = teclado.nextInt();
-        int arreglo[] = new int[tam];
+        System.out.println("---MENU---");
+        System.out.println("1.-ORDENAMIENTO DATOS QUITSHORT");
+        System.out.println("2.INGRESO MATRIZ");
+        op = teclado.nextInt();
+        do {
 
-        // Ingresando valores al arreglo
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print("A[" + i + "]= ");
-            arreglo[i] = teclado.nextInt();
-        }
+            switch (op) {
 
-        quicksort(arreglo, 0, tam - 1);
+                case 1:
 
-        
-        System.out.println("El arreglo oredenado en Quick Sort es: ");
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print("\t" + arreglo[i]);
-        }
+                    System.out.print("Ingrese la dimension del arreglo: ");
+                    int tam = teclado.nextInt();
+                    int arreglo[] = new int[tam];
+
+                    // Ingresando valores al arreglo
+                    for (int i = 0; i < arreglo.length; i++) {
+                        System.out.print("A[" + i + "]= ");
+                        arreglo[i] = teclado.nextInt();
+                    }
+
+                    quicksort(arreglo, 0, tam - 1);
+
+                    System.out.println("El arreglo oredenado en Quick Sort es: ");
+                    for (int i = 0; i < arreglo.length; i++) {
+                        System.out.print("\t" + arreglo[i]);
+                    }
+                    break;
+                case 2:
+                    BufferedReader br;
+
+                    br = new BufferedReader(new InputStreamReader(System.in));
+
+                    System.out.println("Ingrese el tamaño de la fila");
+                    fila = Integer.parseInt(br.readLine());
+
+                    System.out.println("Ingrese el tamaño de la columna");
+                    columna = Integer.parseInt(br.readLine());
+                    a = new int[fila][columna];
+
+                    System.out.println("Ingrese los datos faltantes en la matriz: ");
+                    for (int i = 0; i < fila; i++) {
+                        for (int j = 0; j < columna; j++) {
+                            System.out.print("A[" + i + "][" + j + "]= ");
+                            a[i][j] = Integer.parseInt(br.readLine());
+                        }
+                    }
+                    System.out.println("Matriz Ingresada:");
+                    for (int i = 0; i < a.length; i++) {
+                        for (int j = 0; j < a[i].length; j++) {
+                            System.out.print(a[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+            }
+            System.out.println("Desea Continiar Con La Ejecucion S/N");
+            continuar = teclado.nextInt();
+        } while (continuar.equals("S"));
 
     }
 
-    // Metodo de ordenamineto Quick Sort
     static void quicksort(int arreglo[], int primero, int ultimo) {
         int central, i, j;
         int pivote;
@@ -49,9 +94,9 @@ public class ordennum {
 
         while (i <= j);
         if (primero < j)
-            quicksort(arreglo, primero, j); 
+            quicksort(arreglo, primero, j);
         if (i < ultimo)
-            quicksort(arreglo, i, ultimo); 
+            quicksort(arreglo, i, ultimo);
     }
 
 }
