6749df154734d16565541a2d0fa540b7b5a48fdd
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2021-07-27 12:11:56 -0500

    Proyecto

--- Proyecto/metodos.java
+++ Proyecto/metodos.java
@@ -2,31 +2,29 @@ package Proyecto;
 
 public class metodos {
      static String add(String num1, String num2) {
-        // Asegúrese de que s1 sea menor o igual que la longitud de s2, por lo que es
-        // conveniente agregar 0 después
+        
+        // Compara el numero
         if (num1.length() > num2.length()) {
             String temp = num1;
             num1 = num2;
             num2 = temp;
         }
         int cha = num2.length() - num1.length();
+        //agrega 0 en el caso de ser de direnete longitud
         for (int i = 0; i < cha; i++) {
-            num1 = '0' + num1; // Agrega 0 al frente de la longitud más pequeña para que los dos números tengan
-                           // la misma longitud
+            num1 = '0' + num1; 
         }
         String result = "";
-        int w = 0; // Definir una variable para almacenar carry
-        // De atrás hacia adelante, agregue poco a poco
+        int w = 0; 
+        //operacion
         for (int i = num2.length() - 1; i >= 0; i--) {
-            // Los caracteres extraídos de s1 y s2 son caracteres, todos menos 48 es el
-            // número de tipo int, más el acarreo es el resultado del bit actual
+        
             int c = num2.charAt(i) + num1.charAt(i) - 96 + w;
-            w = c / 10; // La suma máxima de dos números de tipo int de un dígito es más de diez, todo
-                        // dividido por 10 es un acarreo
+            w = c / 10; 
+            //regla dividimos todos los numero invitados para 10 
             result = (c % 10) + result;
         }
-        // Debido a que el ciclo anterior no juzga el acarreo cuando se agrega al primer
-        // lugar, así que juzgalo al final
+        
         if (w == 1)
             result = 1 + result;
         return result;
--- Proyecto/p1.java
+++ Proyecto/p1.java
@@ -4,48 +4,19 @@ import java.util.Scanner;
 
 public class p1 {
     public static void main(String[] args) {
-        Scanner dato = new Scanner(System.in);
+        Scanner datos = new Scanner(System.in);
         // Ingrese dos números grandes de tipo de cadena
         metodos li = new metodos();
 
         System.out.println("Ingrese el primer entero");
-        String num1 = dato.nextLine();
+        String num1 = datos.nextLine();
         System.out.println("Ingrese el segundo entero");
-        String num2 = dato.nextLine();
-        String result = li.add(num1, num2));
+        String num2 = datos.nextLine();
+        String result = li.add(num1, num2);
         System.out.println("El Resultado de la suma de enteros es:  ");
         System.out.println(result);
     }
     
-    //private static String add(String s1, String s2) {
-        // Asegúrese de que s1 sea menor o igual que la longitud de s2, por lo que es
-        // conveniente agregar 0 después
-      //  if (s1.length() > s2.length()) {
-          //  String temp = s1;
-        //    s1 = s2;
-            //s2 = temp;
-        //}
-        //int cha = s2.length() - s1.length();
-        //for (int i = 0; i < cha; i++) {
-        //    s1 = '0' + s1; // Agrega 0 al frente de la longitud más pequeña para que los dos números tengan
-                           // la misma longitud
-        //}
-        //String result = "";
-        //int w = 0; // Definir una variable para almacenar carry
-        // De atrás hacia adelante, agregue poco a poco
-        //for (int i = s2.length() - 1; i >= 0; i--) {
-            // Los caracteres extraídos de s1 y s2 son caracteres, todos menos 48 es el
-            // número de tipo int, más el acarreo es el resultado del bit actual
-        //    int c = s2.charAt(i) + s1.charAt(i) - 96 + w;
-        //    w = c / 10; // La suma máxima de dos números de tipo int de un dígito es más de diez, todo
-                        // dividido por 10 es un acarreo
-        //    result = (c % 10) + result;
-        //}
-        // Debido a que el ciclo anterior no juzga el acarreo cuando se agrega al primer
-        // lugar, así que juzgalo al final
-        //if (w == 1)
-        //    result = 1 + result;
-        ///return result;
-    //}
+    
 
 }
